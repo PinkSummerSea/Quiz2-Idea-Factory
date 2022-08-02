f43282f79056c8001e8344fae0b95db42e8a2a5c
Xiaojun Lin <xiaojunlin@XiaojundeMacBook-Pro.local>
2022-08-02 12:00:40 -0700

    ideas CRUD working. user authentication and authorization working

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :ideas
+  resources :users, only: [:new, :create]
+  resource :session, only: [:new, :create, :destroy]
+  root "ideas#index"
 end
